c5a3618d8f597c4f76e43772dcc79ed2492a5940
tobezhou33 <1339725543@qq.com>
2019-09-25 17:03:00 +0800

    add

---
+++ src/main/java/com/seu/miaosha/controller/MiaoshaController.java
@@ -0,0 +1,65 @@
+package com.seu.miaosha.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.seu.miaosha.domain.MiaoshaOrder;
+import com.seu.miaosha.domain.MiaoshaUser;
+import com.seu.miaosha.domain.OrderInfo;
+import com.seu.miaosha.redis.RedisService;
+import com.seu.miaosha.result.CodeMsg;
+import com.seu.miaosha.service.GoodsService;
+import com.seu.miaosha.service.MiaoshaService;
+import com.seu.miaosha.service.MiaoshaUserService;
+import com.seu.miaosha.service.OrderService;
+import com.seu.miaosha.vo.GoodsVo;
+
+@Controller
+@RequestMapping("/miaosha")
+public class MiaoshaController {
+
+	@Autowired
+	MiaoshaUserService userService;
+	
+	@Autowired
+	RedisService redisService;
+	
+	@Autowired
+	GoodsService goodsService;
+	
+	@Autowired
+	OrderService orderService;
+	
+	@Autowired
+	MiaoshaService miaoshaService;
+	
+    @RequestMapping("/do_miaosha")
+    public String list(Model model,MiaoshaUser user,
+    		@RequestParam("goodsId")long goodsId) {
+    	model.addAttribute("user", user);
+    	if(user == null) {
+    		return "login";
+    	}
+    	//判断库存
+    	GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+    	int stock = goods.getStockCount();
+    	if(stock <= 0) {
+    		model.addAttribute("errmsg", CodeMsg.MIAO_SHA_OVER.getMsg());
+    		return "miaosha_fail";
+    	}
+    	//判断是否已经秒杀到了
+    	MiaoshaOrder order = orderService.getMiaoshaOrderByUserIdGoodsId(user.getId(), goodsId);
+    	if(order != null) {
+    		model.addAttribute("errmsg", CodeMsg.REPEATE_MIAOSHA.getMsg());
+    		return "miaosha_fail";
+    	}
+    	//减库存 下订单 写入秒杀订单
+    	OrderInfo orderInfo = miaoshaService.miaosha(user, goods);
+    	model.addAttribute("orderInfo", orderInfo);
+    	model.addAttribute("goods", goods);
+        return "order_detail";
+    }
+}
---
+++ src/main/java/com/seu/miaosha/service/MiaoshaService.java
@@ -0,0 +1,28 @@
+package com.seu.miaosha.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.seu.miaosha.domain.MiaoshaUser;
+import com.seu.miaosha.domain.OrderInfo;
+import com.seu.miaosha.vo.GoodsVo;
+
+@Service
+public class MiaoshaService {
+	
+	@Autowired
+	GoodsService goodsService;
+	
+	@Autowired
+	OrderService orderService;
+
+	@Transactional
+	public OrderInfo miaosha(MiaoshaUser user, GoodsVo goods) {
+		//减库存 下订单 写入秒杀订单
+		goodsService.reduceStock(goods);
+		//order_info maiosha_order
+		return orderService.createOrder(user, goods);
+	}
+	
+}
