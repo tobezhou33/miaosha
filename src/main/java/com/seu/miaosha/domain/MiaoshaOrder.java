50551e73df2cbe3b5f22be92168a82c990b1e1c4
tobezhou33 <1339725543@qq.com>
2019-09-25 10:01:05 +0800

    add goods

---
+++ src/main/java/com/seu/miaosha/domain/MiaoshaOrder.java
@@ -0,0 +1,13 @@
+package com.seu.miaosha.domain;
+
+
+import lombok.Data;
+
+@Data
+public class MiaoshaOrder {
+	private Long id;
+	private Long userId;
+	private Long  orderId;
+	private Long goodsId;
+
+}
