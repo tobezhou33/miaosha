8b1715d488fc7738da66b9702c932651ffd9ccd3
tobezhou33 <1339725543@qq.com>
2019-09-26 21:42:33 +0800

    miaosha5

--- src/main/java/com/seu/miaosha/dao/MiaoshaUserDao.java
+++ src/main/java/com/seu/miaosha/dao/MiaoshaUserDao.java
@@ -5,10 +5,16 @@ import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.seu.miaosha.domain.MiaoshaUser;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface MiaoshaUserDao {
 	
 	@Select("select * from miaosha_user where id = #{id}")
 	MiaoshaUser getById(@Param("id") long id);
+
+	@Update("update miaosha_user set password = #{password} where id = #{id}")
+	public void update(MiaoshaUser toBeUpdate);
+
+
 }
