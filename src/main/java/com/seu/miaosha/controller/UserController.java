f8ab3c70a0b2d0be28cc748352d5ef575721155f
tobezhou33 <1339725543@qq.com>
2019-09-26 13:46:32 +0800

    miaosha4

---
+++ src/main/java/com/seu/miaosha/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.seu.miaosha.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.seu.miaosha.domain.MiaoshaUser;
+import com.seu.miaosha.redis.RedisService;
+import com.seu.miaosha.result.Result;
+import com.seu.miaosha.service.MiaoshaUserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	MiaoshaUserService userService;
+	
+	@Autowired
+	RedisService redisService;
+	
+    @RequestMapping("/info")
+    @ResponseBody
+    public Result<MiaoshaUser> info(Model model,MiaoshaUser user) {
+        return Result.success(user);
+    }
+    
+}
